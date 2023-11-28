c5ed976899eb3c9e1531a1195a20806ccc385a66
jose francicso valdez <josevaldez@bcp.com.pe>
2023-11-27 21:06:20 -0300

    feat:Add test

---
+++ src/test/java/com/example/r2dgbc/demo/services/DepartmentServiceTest.java
@@ -0,0 +1,259 @@
+package com.example.r2dgbc.demo.services;
+
+
+import com.example.r2dgbc.demo.controller.dto.request.CreateDepartmentRequest;
+import com.example.r2dgbc.demo.exceptions.DepartmentAlreadyExistsException;
+import com.example.r2dgbc.demo.exceptions.DepartmentNotFoundException;
+import com.example.r2dgbc.demo.repository.DepartmentRepository;
+import com.example.r2dgbc.demo.repository.entity.Department;
+import com.example.r2dgbc.demo.repository.entity.Employee;
+import com.example.r2dgbc.demo.service.impl.DepartamentServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class DepartmentServiceTest {
+    @Mock
+    private DepartmentRepository repository;
+    @InjectMocks
+    private DepartamentServiceImpl departmantService;
+
+
+
+    private Department mockDevDepartment() {
+        return Department.builder()
+                .id(1L)
+                .name("Software Development")
+                .manager(Employee.builder()
+                        .id(1L)
+                        .firstName("Jose")
+                        .lastName("valdez")
+                        .position("Software Development BCP")
+                        .fullTime(true)
+                        .build())
+                .employees(List.of(
+                        Employee.builder()
+                                .id(2L)
+                                .firstName("jesus")
+                                .lastName("mendoza")
+                                .position("Chapter")
+                                .fullTime(true)
+                                .build(),
+                        Employee.builder()
+                                .id(3L)
+                                .firstName("eric")
+                                .lastName("llano")
+                                .position("chapeter")
+                                .fullTime(false)
+                                .build()))
+                .build();
+    }
+
+    private Department mockdRHDepartment() {
+        return Department.builder()
+                .id(2L)
+                .name("RRHH")
+                .manager(Employee.builder()
+                        .id(4L)
+                        .firstName("Angela")
+                        .lastName("Perez")
+                        .position("Director of Human Resources")
+                        .fullTime(true)
+                        .build())
+                .employees(List.of(
+                        Employee.builder()
+                                .id(5L)
+                                .firstName("Diana")
+                                .lastName("Ventura")
+                                .position("Data Analyst")
+                                .fullTime(true)
+                                .build()))
+                .build();
+    }
+
+
+
+    @Test
+    @DisplayName("Test 2 petición departments(3) should throw DepartmentNotFoundException")
+    void test_GetDepartment_Should_ThrowDepartment_NotFound() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        this.departmantService.getDepartment(3L)
+                .as(StepVerifier::create)
+                .expectError(DepartmentNotFoundException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("Test 3 petición gepartmentgetDepartmentEmployees(1, null) should return 2 Employees")
+    void test_GetDepartmentEmployees_should_GetDepartmentEmployees_When_TwoEmployees() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockDevDepartment()));
+
+        this.departmantService.getDepartmentEmployees(1L, null)
+                .as(StepVerifier::create)
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 1 petición departments() should return 2 Departments")
+    void test_GetDepartments_should_GetDepartments_When_TwoDepartments() {
+        when(this.repository.findAll()).thenReturn(Flux.fromIterable(List.of(mockDevDepartment(), mockdRHDepartment())));
+
+        this.departmantService.getDepartments()
+                .as(StepVerifier::create)
+                .expectNextCount(2)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 2 petición departments() should return 1 Department")
+    void test_GetDepartment_should_GetDepartments_When_OneDepartments() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockDevDepartment()));
+
+        this.departmantService.getDepartment(1L)
+                .as(StepVerifier::create)
+                .consumeNextWith(department -> assertEquals(mockDevDepartment(), department))
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 4 petición getEmployeesgetDepartmentEmployees(1, true) should return 1 Employees")
+    void test_GetDepartmentEmployees_should_GetDepartmentEmployees_When_OneEmployee() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockDevDepartment()));
+
+        this.departmantService.getDepartmentEmployees(1L, true)
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+
+
+    @Test
+    @DisplayName("Test 7 petición createDepartment(request) should return a Department")
+    void test_createDepartment_should_createDepartment_When_ReturnDepartment() {
+        Department accounting = Department.builder()
+                .id(3L)
+                .name("Accounting")
+                .build();
+
+        when(this.repository.findByName(anyString())).thenReturn(Mono.empty());
+        when(this.repository.save(any(Department.class))).thenReturn(Mono.just(accounting));
+
+        this.departmantService.createDepartment(new CreateDepartmentRequest("Accounting"))
+                .as(StepVerifier::create)
+                .consumeNextWith(department -> assertEquals(accounting, department))
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 5 petición getDepartmentEmployees(1, false) should return 1 Employees")
+    void test_GetDepartmentEmployees_should_NoIsFullTime_GetDepartmentEmployees_When_OneEmployee() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockDevDepartment()));
+
+        this.departmantService.getDepartmentEmployees(1L, false)
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 6 petición getDepartmentEmployees(3, null) should throw DepartmentNotFoundException")
+    void test_GetDepartmentEmployees_should_ThrowDepartment_NotFound() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        this.departmantService.getDepartmentEmployees(3L, null)
+                .as(StepVerifier::create)
+                .expectError(DepartmentNotFoundException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("Test 8 petición createDepartment(request) should throw DepartmentAlreadyExistsException")
+    void test_createDepartment_should_ThrowDepartment_AlreadyExists() {
+        Department accounting = Department.builder()
+                .id(3L)
+                .name("Accounting")
+                .build();
+
+        when(this.repository.findByName(anyString())).thenReturn(Mono.just(accounting));
+
+        this.departmantService.createDepartment(new CreateDepartmentRequest("Accounting"))
+                .as(StepVerifier::create)
+                .expectError(DepartmentAlreadyExistsException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("Test 9 petición updateDepartment(1, department) should return an updated Department")
+    void test_UpdateDepartment_Should_UpdateDepartment_ReturnDepartment() {
+        Department updatedDevDepartment = mockDevDepartment();
+
+        Employee manager = Employee.builder()
+                .id(6L)
+                .firstName("Sally")
+                .lastName("Smith")
+                .position("Director of Software Development")
+                .fullTime(true)
+                .build();
+
+        updatedDevDepartment.setManager(manager);
+
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockDevDepartment()));
+        when(this.repository.save(any(Department.class))).thenReturn(Mono.just(updatedDevDepartment));
+
+        this.departmantService.updateDepartment(1L, updatedDevDepartment)
+                .as(StepVerifier::create)
+                .consumeNextWith(department -> assertEquals(updatedDevDepartment, department))
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 10 petición updateDepartment(3, department) should throw DepartmentNotFoundException")
+    void test_updateDepartment_should_updateDepartment_When_DepartmentNotFound() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        this.departmantService.updateDepartment(3L, mockDevDepartment())
+                .as(StepVerifier::create)
+                .expectError(DepartmentNotFoundException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("Test 11 petición deleteDepartment(1) should complete")
+    void test_deleteDepartment_should_deleteDepartment_when_DeleteDepartment() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockDevDepartment()));
+        when(this.repository.delete(any(Department.class))).thenReturn(Mono.empty());
+
+        this.departmantService.deleteDepartment(mockDevDepartment().getId())
+                .as(StepVerifier::create)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 12  petición deleteDepartment(3) should throw DepartmentNotFound")
+    void test_DeleteDepartment_should_DeleteDepartment_when_ThrowDepartmentNotFound() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        this.departmantService.deleteDepartment(3L)
+                .as(StepVerifier::create)
+                .expectError(DepartmentNotFoundException.class)
+                .verify();
+    }
+
+
+}
---
+++ src/test/java/com/example/r2dgbc/demo/services/EmployeeServiceTest.java
@@ -0,0 +1,179 @@
+package com.example.r2dgbc.demo.services;
+
+
+import com.example.r2dgbc.demo.controller.dto.request.CreateEmployeeRequest;
+import com.example.r2dgbc.demo.exceptions.EmployeeNotFoundException;
+import com.example.r2dgbc.demo.repository.EmployeeRepository;
+import com.example.r2dgbc.demo.repository.entity.Employee;
+import com.example.r2dgbc.demo.service.impl.EmployeeServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class EmployeeServiceTest {
+    @Mock
+    private EmployeeRepository repository;
+    @InjectMocks
+    private EmployeeServiceImpl service;
+
+
+
+    private Employee mockEmployee() {
+        return Employee.builder()
+                .id(1L)
+                .firstName("jose")
+                .lastName("valdez")
+                .position("Software Developer")
+                .fullTime(true)
+                .build();
+    }
+
+
+
+    @Test
+    @DisplayName("Test 3 petición getEmployees(null, true) Should return 1 Employee")
+    void test_GetEmployeesByFullTime_Should_GetEmployeesByFullTime_ReturnEmployees() {
+        when(this.repository.findAllByFullTime(anyBoolean())).thenReturn(Flux.just(mockEmployee()));
+
+        this.service.getEmployees(null, true)
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 4 petición getEmployees(\"Software Developer\", true) Should return 1 Employee")
+    void test_GetEmployeesByPositionAndFullTime_Should_GetEmployeesByPositionAndFullTime_ReturnEmployees() {
+        when(this.repository.findAllByPositionAndFullTime(anyString(), anyBoolean())).thenReturn(Flux.just(mockEmployee()));
+
+        this.service.getEmployees("Software Developer", true)
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 1 petición getEmployees(null, null) Should return 1 Employee")
+    void test_GetEmployees_Should_GetEmployees_ReturnEmployees() {
+        when(this.repository.findAll()).thenReturn(Flux.just(mockEmployee()));
+
+        this.service.getEmployees(null, null)
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 2  petición getEmployees(\"Software Developer\", null) Should return 1 Employee")
+    void test_GetEmployeesByPosition_Should_GetEmployeesByPosition_ReturnEmployees() {
+        when(this.repository.findAllByPosition(anyString())).thenReturn(Flux.just(mockEmployee()));
+
+        this.service.getEmployees("Software Developer", null)
+                .as(StepVerifier::create)
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+
+    @Test
+    @DisplayName("Test 7 petición createEmployee(request) Should return an Employee")
+    void test_CreateEmployee_Should_CreateEmployee_ReturnEmployee() {
+        Employee newEmployee = Employee.builder()
+                .firstName("Bob")
+                .lastName("Walker")
+                .position("Dog Walker")
+                .fullTime(false)
+                .build();
+
+        when(this.repository.save(any(Employee.class))).thenReturn(Mono.just(newEmployee));
+
+        this.service.createEmployee(new CreateEmployeeRequest("Bob", "Walker", "Dog Walker", false))
+                .as(StepVerifier::create)
+                .consumeNextWith(employee -> assertEquals(newEmployee, employee))
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 8 petición updateEmployee(1, employee) Should return an updated Employee")
+    void test_UpdateEmployee_Should_UpdateEmployee_ReturnEmployee() {
+        Employee updatedEmployee = mockEmployee();
+
+        updatedEmployee.setFirstName("George");
+
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockEmployee()));
+        when(this.repository.save(any(Employee.class))).thenReturn(Mono.just(updatedEmployee));
+
+        this.service.updateEmployee(1L, updatedEmployee)
+                .as(StepVerifier::create)
+                .consumeNextWith(employee -> assertEquals(updatedEmployee, employee))
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 9 petición updateEmployee(2, employee) Should throw EmployeeNotFoundException")
+    void test_UpdateEmployee_Should_UpdateEmployee_ThrowEmployeeNotFound() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        this.service.updateEmployee(2L, mockEmployee())
+                .as(StepVerifier::create)
+                .expectError(EmployeeNotFoundException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("Test 5 petición getEmployee(1) Should return an Employee")
+    void test_GetEmployee_Should_Return_GetEmployee_Employee() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockEmployee()));
+
+        this.service.getEmployee(1L)
+                .as(StepVerifier::create)
+                .consumeNextWith(employee -> assertEquals(mockEmployee(), employee))
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 6 petición getEmployee(2) Should throw EmployeeNotFoundException")
+    void test_GetEmployee_Should_GetEmployee_ThrowEmployeeNotFound() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        this.service.getEmployee(2L)
+                .as(StepVerifier::create)
+                .expectError(EmployeeNotFoundException.class)
+                .verify();
+    }
+
+    @Test
+    @DisplayName("Test 10 petición deleteEmployee(1) Should complete")
+    void test_DeleteEmployee_Should_DeleteEmployee_DeleteEmployee() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.just(mockEmployee()));
+        when(this.repository.delete(any(Employee.class))).thenReturn(Mono.empty());
+
+        this.service.deleteEmployee(1L)
+                .as(StepVerifier::create)
+                .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Test 11 petición deleteEmployee(2) Should throw EmployeeNotFoundException")
+    void test_DeleteEmployee_Should_DeleteEmployee_ReturnEmployeeNotFound() {
+        when(this.repository.findById(anyLong())).thenReturn(Mono.empty());
+
+        this.service.deleteEmployee(2L)
+                .as(StepVerifier::create)
+                .expectError(EmployeeNotFoundException.class)
+                .verify();
+    }
+
+
+}
